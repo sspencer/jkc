8dcb408309f3ec83dca6ea344c5187ae67bd938e
Steve Spencer <steve@bigfrog.net>
2022-03-04 15:35:32 -0800

    Type is now guess as the most frequently appearing type, rather than last type seen.

--- README.md
+++ README.md
@@ -10,34 +10,35 @@ directories to count unique key paths found in *.json files.
 3. Keys may be excluded to reduce noise with `-v key` flag
 4. IDs (UUID and PushIds) may be rolled up with the `-i` flag which reduces them to `<id>`
 5. Partial keys can be ignored with the `-p` flag
-6. Reports with directory crawled may be printed with -csv or -tsv flags.
+6. The type of each key is reported (string, number, boolean, unknown).  Objects and arrays are flattened or rolled up. 
+7. Reports with directory crawled may be printed with -csv or -tsv flags.
 
 ## Usage 
 
 ```
 USAGE:
   jkc <dir1> [<dir2> <dir3> ...]
-  -csv 	    output in CSV format
-  -i	    skip keys that looks like push ids or uuids
-  -p value 	skip "key" with this substring (invert match)
-  -tsv    	output in TSV format
-  -v value 	skip this "key" (invert match)```
+  -csv      output in CSV format
+  -i        skip keys that looks like push ids or uuids
+  -p value  skip "key" with this substring (invert match)
+  -tsv      output in TSV format
+  -v value  skip this "key" (invert match)```
 ```
 
 ```
 $ jkc -i -v type testdata
-pets[*].<id>.animal.name  string   2
-pets[*].<id>.animal.type  string   2
-pets[*].<id>.type         string   2
-pets[*].bark              boolean  6
-pets[*].meow              boolean  1
-pets[*].name              string   7
-pets[*].speak             boolean  3
-pets[*].type              string   7
+pets[*].<id>.animal.name  string      2
+pets[*].<id>.animal.type  string      2
+pets[*].<id>.type         string      2
+pets[*].bark              boolean(*)  6
+pets[*].meow              boolean     1
+pets[*].name              string      7
+pets[*].speak             boolean     3
+pets[*].type              string(*)   7
+
 ```
 
 ## Notes
 
-You may skip more than one key:
-
-    jkc -v input -v output dir1 dir2 dir3
+* You may skip more than one key: `jkc -v input -v output dir1 dir2 dir3`
+* If a type ends with (*), it means that the field may have more than one type.
--- main.go
+++ main.go
@@ -20,10 +20,11 @@ type config struct {
 	skipIds      bool
 }
 
-type countType struct {
-	count int
-	typ3  string // since 'type' is a reserved word
-	dir   string
+type field struct {
+	keyCount  int
+	typeCount map[string]int // count how many times each type appears
+	typeGuess string         // best guess of the type
+	dir       string         // directory name (hint: name it same as "entity")
 }
 
 type arrayFlags []string
@@ -60,7 +61,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	countMap := make(map[string]countType)
+	countMap := make(map[string]field)
 
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -96,7 +97,7 @@ func main() {
 }
 
 // recursively walk directory looking for all *.json files
-func walkDir(dir string, countMap map[string]countType, cfg config) error {
+func walkDir(dir string, countMap map[string]field, cfg config) error {
 	fileSystem := os.DirFS(dir)
 
 	return fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
@@ -114,8 +115,8 @@ func walkDir(dir string, countMap map[string]countType, cfg config) error {
 	})
 }
 
-// read json file, count unique key paths
-func countPaths(dir, path string, countMap map[string]countType, cfg config) error {
+// read json file, keyCount unique key paths
+func countPaths(dir, path string, countMap map[string]field, cfg config) error {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return err
@@ -149,8 +150,11 @@ func countPaths(dir, path string, countMap map[string]countType, cfg config) err
 		key := normalizeKeyPath(keyPath, cfg)
 		if len(key) > 0 {
 			ct := countMap[key]
-			ct.count++
-			ct.typ3 = typ
+			ct.keyCount++
+			if ct.typeCount == nil {
+				ct.typeCount = make(map[string]int)
+			}
+			ct.typeCount[typ]++
 			ct.dir = dir
 			countMap[key] = ct
 		}
@@ -215,15 +219,16 @@ func looksLikeId(key string) bool {
 	return false
 }
 
-// Sort map keys and align count based on longest key path
-func prettyPrint(m map[string]countType) {
+// Sort map keys and align keyCount based on longest key path
+func prettyPrint(m map[string]field) {
 	var maxLenKey, maxLenType int
+	guessTypes(m)
 	keys := make([]string, 0, len(m))
 	for k, ct := range m {
 		if len(k) > maxLenKey {
 			maxLenKey = len(k)
 		}
-		countLen := len(ct.typ3)
+		countLen := len(ct.typeGuess)
 		if countLen > maxLenType {
 			maxLenType = countLen
 		}
@@ -234,11 +239,26 @@ func prettyPrint(m map[string]countType) {
 
 	for _, k := range keys {
 		ct := m[k]
-		fmt.Printf("%s%s  %s%s  %d\n", k, strings.Repeat(" ", maxLenKey-len(k)), ct.typ3, strings.Repeat(" ", maxLenType-len(ct.typ3)), ct.count)
+		fmt.Printf("%s%s  %s%s  %d\n", k, strings.Repeat(" ", maxLenKey-len(k)), ct.typeGuess, strings.Repeat(" ", maxLenType-len(ct.typeGuess)), ct.keyCount)
 	}
 }
 
-func sortKeys(m map[string]countType) []string {
+func csvPrint(m map[string]field, separator rune) {
+	guessTypes(m)
+	keys := sortKeys(m)
+	w := csv.NewWriter(os.Stdout)
+	w.Comma = separator
+	for _, k := range keys {
+		ct := m[k]
+		if err := w.Write([]string{k, ct.typeGuess, ct.dir, strconv.Itoa(ct.keyCount)}); err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	w.Flush()
+}
+
+func sortKeys(m map[string]field) []string {
 	keys := make([]string, 0, len(m))
 	for k := range m {
 
@@ -248,16 +268,30 @@ func sortKeys(m map[string]countType) []string {
 	return keys
 }
 
-func csvPrint(m map[string]countType, separator rune) {
-	keys := sortKeys(m)
-	w := csv.NewWriter(os.Stdout)
-	w.Comma = separator
-	for _, k := range keys {
-		ct := m[k]
-		if err := w.Write([]string{k, ct.typ3, ct.dir, strconv.Itoa(ct.count)}); err != nil {
-			log.Fatalln(err)
+func guessTypes(m map[string]field) {
+	for fieldKey, fieldVal := range m {
+		type kv struct {
+			typeKey string
+			typeCnt int
 		}
-	}
+		var ss []kv
+		for k, v := range fieldVal.typeCount {
+			ss = append(ss, kv{k, v})
+		}
 
-	w.Flush()
+		sort.Slice(ss, func(i, j int) bool {
+			return ss[i].typeCnt > ss[j].typeCnt
+		})
+
+		jk := m[fieldKey]
+		if len(ss) == 0 {
+			jk.typeGuess = "unknown"
+		} else if len(ss) == 1 {
+			jk.typeGuess = ss[0].typeKey
+		} else {
+			jk.typeGuess = ss[0].typeKey + "(*)"
+		}
+
+		m[fieldKey] = jk
+	}
 }
